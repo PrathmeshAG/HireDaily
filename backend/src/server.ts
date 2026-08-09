3ade68e3c472f8cc2a2749f3763f571215924fea
bobad <prathmeshbobade33@gmail.com>
2026-08-09 17:04:57 +0530

    isssue solve

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
-import rateLimit from "express-rate-limit";
+import { rateLimit } from "express-rate-limit";
 
 import { parseWebhookEvents } from "./utils/webhook-parser.js";
 import {
